a653b7d736a8e499c68e0cf040acfd5c3032d09a
edenlisk <nkumbuyenock@gmail.com>
2023-01-31 15:11:56 +0200

    Fix eslint errors

--- src/redux/book/book.js
+++ src/redux/book/book.js
@@ -1,29 +1,24 @@
 import { createAction } from '@reduxjs/toolkit/src/createAction';
-import { createReducer } from "@reduxjs/toolkit/src/createReducer";
+import { createReducer } from '@reduxjs/toolkit/src/createReducer';
 import { nanoid } from '@reduxjs/toolkit';
-const ADD_BOOK = createAction('bookstore/books/ADD_BOOK', (book) => {
-  return {
-    payload: {
-      id: nanoid(),
-      title: book.title,
-      author: book.author,
-      completed: false,
-    }
-  }
-});
+
+const ADD_BOOK = createAction('bookstore/books/ADD_BOOK', (book) => ({
+  payload: {
+    id: nanoid(),
+    title: book.title,
+    author: book.author,
+    completed: false,
+  },
+}));
 const REMOVE_BOOK = createAction('bookstore/books/REMOVE_BOOK');
 
 const INITIAL_STATE = [];
 
-const bookReducer = createReducer(INITIAL_STATE, (builder => {
+const bookReducer = createReducer(INITIAL_STATE, ((builder) => {
   builder
-      .addCase(ADD_BOOK, (state, action) => {
-        return [...state, action.payload];
-      })
-      .addCase(REMOVE_BOOK, (state, action) => {
-        return [state.filter((book) => book !== action.payload)];
-      })
-      .addDefaultCase(state => state);
-}))
+    .addCase(ADD_BOOK, (state, action) => [...state, action.payload])
+    .addCase(REMOVE_BOOK, (state, action) => [state.filter((book) => book !== action.payload)])
+    .addDefaultCase((state) => state);
+}));
 
 export default bookReducer;
--- src/redux/categories/categories.js
+++ src/redux/categories/categories.js
@@ -1,14 +1,13 @@
 import { createAction } from '@reduxjs/toolkit/src/createAction';
-import { createReducer } from "@reduxjs/toolkit/src/createReducer";
+import { createReducer } from '@reduxjs/toolkit/src/createReducer';
 
 export const CHECK_STATUS = createAction('bookstore/categories/CHECK_STATUS');
 const INITIAL_STATE = [];
 
-const categoriesReducer = createReducer(INITIAL_STATE, (builder => {
+const categoriesReducer = createReducer(INITIAL_STATE, ((builder) => {
   builder
-      .addCase(CHECK_STATUS,() => 'Under Construction')
-      .addDefaultCase(state => state);
-}))
-
+    .addCase(CHECK_STATUS, () => 'Under Construction')
+    .addDefaultCase((state) => state);
+}));
 
 export default categoriesReducer;
